313bca7854d63ea2d28f04391cdb712cc92df8ac
Masaya NISHIDA <nishida@rnum.org>
2022-02-20 21:29:22 +0900

    1枚のシートに複数写真を載せられるように修正

--- src/slideshow_from_drop.py
+++ src/slideshow_from_drop.py
@@ -10,44 +10,48 @@ from pptx.dml.color import RGBColor
 from pptx.slide import Slides
 
 
-def add_slide(p: Presentation()) -> Slides:
+def add_slide(prs: Presentation()) -> Slides:
     # 白紙スライドの追加(ID=6は白紙スライド)
-    blank_slide_layout = p.slide_layouts[6]
-    slide = p.slides.add_slide(blank_slide_layout)
+    blank_slide_layout = prs.slide_layouts[6]
+    slide = prs.slides.add_slide(blank_slide_layout)
     return slide
 
 
-def add_picture(slide: Slides, img_path: str, slide_shape: dict) -> Slides:
+def add_picture(img_path: str, prs, slide: Slides, grid: tuple, position:int) -> Slides:
+    slide_width = prs.slide_width
+    slide_height = prs.slide_height
+    grid_width, grid_height = (int(slide_width / grid[1]), int(slide_height / grid[0]))
+    grid_aspect_ratio = grid_width / grid_height
+
     # 画像サイズを取得してアスペクト比を得る
     im = Image.open(img_path)
     im_width, im_height = im.size
-    aspect_ratio = im_width / im_height
+    im_aspect_ratio = im_width / im_height
 
-    slide_aspect_ratio = slide_shape['width']/slide_shape['height']
-    if aspect_ratio > slide_aspect_ratio:  # 画像のほうが横長の場合
-        img_display_width = slide_shape['width']
-        img_display_height = img_display_width / aspect_ratio
+    if im_aspect_ratio > grid_aspect_ratio:  # 画像のほうが横長の場合
+        img_display_width = grid_width
+        img_display_height = img_display_width / im_aspect_ratio
     else:  # 画像のほうが縦長の場合
-        img_display_height = slide_shape['height']
-        img_display_width = img_display_height * aspect_ratio
-    # センタリングする場合の画像の左上座標を計算
-    img_center_x = slide_shape['width'] / 2
-    img_center_y = slide_shape['height'] / 2
-    left = img_center_x - img_display_width / 2
-    top = img_center_y - img_display_height / 2
+        img_display_height = grid_height
+        img_display_width = img_display_height * im_aspect_ratio
+    # グリッドの左上座標を計算
+    grid_left = grid_width * (position % grid[1])
+    grid_top = grid_height * int(position / grid[1])
+    left = grid_left + ((grid_width - img_display_width) / 2)
+    top = grid_top + ((grid_height - img_display_height) / 2)
 
     # 画像をスライドに追加
-    if aspect_ratio > slide_aspect_ratio:
-        slide.shapes.add_picture(img_path, left, top, width=img_display_width)
-    else:
-        slide.shapes.add_picture(img_path, left, top, height=img_display_height)
+    slide.shapes.add_picture(img_path, left, top, width=img_display_width)
 
     return slide
 
 
 # ファイル名をスライド右上に貼り付ける
-def add_filename(slide: Slides, file_name: str, slide_shape: dict):
-    text_box = slide.shapes.add_textbox(0, 0, slide_shape['width'], Pt(28))
+def add_filename(file_name: str, prs, slide: Slides, slide_shape: dict):
+    slide_width = prs.slide_width
+    slide_height = prs.slide_height
+
+    text_box = slide.shapes.add_textbox(0, 0, slide_width, Pt(28))
     # text_box.fill.solid()
     # text_box.fill.fore_color.rgb = RGBColor(0, 0, 0)
     text_frame = text_box.text_frame
@@ -59,7 +63,7 @@ def add_filename(slide: Slides, file_name: str, slide_shape: dict):
     return slide
 
 
-def make_slideshow(img_file_paths: list, slide_aspect_ratio: float = 4 / 3):
+def make_slideshow(img_file_paths: list, slide_aspect_ratio: float = 4 / 3, grid: tuple = (1, 1)):
     """
     4:3 (default) 9144000x6858000, 16:9 9144000x5143680
     Unit: English Metric Units = 1/360000 of centimeter
@@ -67,6 +71,7 @@ def make_slideshow(img_file_paths: list, slide_aspect_ratio: float = 4 / 3):
     Args:
         img_file_paths: list of image file paths (full path)
         slide_aspect_ratio: slide width/height
+        grid: image grid numbers as tuple (row, column)
 
     Returns:
         No returns.
@@ -82,21 +87,27 @@ def make_slideshow(img_file_paths: list, slide_aspect_ratio: float = 4 / 3):
     # region Initialize: Make Slide object, Set output filename
     slide_width = 9144000
     slide_height = int(slide_width / slide_aspect_ratio)  # 6858000
-    ppt = Presentation()
-    ppt.slide_width = slide_width
-    ppt.slide_height = slide_height
+    # Generate presentation object
+    prs = Presentation()
+    prs.slide_width = slide_width
+    prs.slide_height = slide_height
+    # Set output params
     output_dir = os.path.dirname(img_file_paths[0])
     print('Output .pptx file dir = ' + output_dir)
     output_file_name = output_dir + '/slideshow_' + datetime.datetime.now().strftime('%Y%m%d_%H%M%S') + '.pptx'
+    # Calculate image size
+    qty_per_a_slide = int(grid[0])*int(grid[1])
     # endregion
 
-    for img_path in img_file_paths:
-        slide = add_slide(ppt)
-        add_picture(slide, img_path, {'width': slide_width, 'height': slide_height})
-        add_filename(slide, os.path.basename(img_path), {'width': slide_width, 'height': slide_height})
+    for i, img_path in enumerate(img_file_paths):
+        if i % qty_per_a_slide == 0:
+            slide = add_slide(prs)
+        position = i % qty_per_a_slide
+        add_picture(img_path, prs, slide, grid, position)
+        #add_filename(os.path.basename(img_path), slide, {'width': slide_width, 'height': slide_height})
 
     # save .pptx file
-    ppt.save(output_file_name)
+    prs.save(output_file_name)
 
 
 if __name__ == '__main__':
